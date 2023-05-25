ef10eee516485a5d87a1b3c68a070454fd027a4a
lembogeorgex <lembogiorgio2@gmail.com>
2023-05-25 15:50:20 +0200

    adding comments

--- js/script.js
+++ js/script.js
@@ -2,19 +2,25 @@
 console.log("JS OK");
 
 // Operations
+
+// Alert message
 alert("In questa pagina verranno utilizzati i tuoi dati per generare una password fittizia");
 
+// Name Question
 const userNameVar= prompt("Qual'è il tuo nome?", "George")
 console.log(userNameVar);
 
+// Surnamer Question
 const userSurnameVar= prompt("Qual'è il tuo cognome?", "Lembo")
 console.log(userSurnameVar);
 
+// Color Question
 const userColorVar= prompt("Qual'è il tuo colore preferito?", "Verde")
 console.log(userColorVar);
 
+// Var Compound
 const userPwdResult = userNameVar + userSurnameVar + userColorVar + "21"
 console.log(userPwdResult);
 
-
+// MOD Show Answer
 document.getElementById("pwdResult").innerText= userPwdResult
