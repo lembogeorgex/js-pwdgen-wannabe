6724cf73c87f87bd55a9dd0a5da99d78335ef866
lembogeorgex <lembogiorgio2@gmail.com>
2023-05-25 15:01:15 +0200

    create mod elements / color prompt var

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 // Console check
-console.log("JS OK")
+console.log("JS OK");
 
 // Operations
+alert("In questa pagina verranno utilizzati i tuoi dati per generare una password fittizia");
 
 const userNameVar= prompt("Qual'è il tuo nome?", "George")
-console.log(userNameVar)
+console.log(userNameVar);
 
 const userSurnameVar= prompt("Qual'è il tuo cognome?", "Lembo")
-console.log(userSurnameVar)
+console.log(userSurnameVar);
+
+const userColorVar= prompt("Qual'è il tuo colore preferito?", "Verde")
+console.log(userColorVar);
